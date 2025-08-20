4adbc3f00d7262611a7ef33508331b337b3b928a
Ekrynox <lucas123.a17@gmail.com>
2025-08-20 20:55:09 +0200

    Plugin init

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+﻿# NINA++
+
+## 1.0.0.1
+- Initial release
---
+++ Nina.cs
@@ -0,0 +1,37 @@
+﻿using LucasAlias.NINA.NinaPP.Properties;
+using NINA.Core.Model;
+using NINA.Core.Utility;
+using NINA.Image.ImageData;
+using NINA.Plugin;
+using NINA.Plugin.Interfaces;
+using NINA.Profile;
+using NINA.Profile.Interfaces;
+using NINA.WPF.Base.Interfaces.Mediator;
+using NINA.WPF.Base.Interfaces.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Settings = LucasAlias.NINA.NinaPP.Properties.Settings;
+
+namespace LucasAlias.NINA.NinaPP {
+    [Export(typeof(IPluginManifest))]
+    public class Nina : PluginBase {
+        private readonly IPluginOptionsAccessor pluginSettings;
+        private readonly IProfileService profileService;
+
+        [ImportingConstructor]
+        public Nina(IProfileService profileService, IOptionsVM options) {
+            this.pluginSettings = new PluginOptionsAccessor(profileService, Guid.Parse(this.Identifier));
+            this.profileService = profileService;
+        }
+
+        public override Task Teardown() {
+            return base.Teardown();
+        }
+    }
+}
