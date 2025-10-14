9384878bcc08ebc96e26f58935607ddca1b9b3eb
Ekrynox <lucas123.a17@gmail.com>
2025-10-14 18:38:31 +0200

    rename repo link

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,18 +31,18 @@ using System.Runtime.InteropServices;
 // The url to the license
 [assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
 // The repository where your pluggin is hosted
-[assembly: AssemblyMetadata("Repository", "https://github.com/Ekrynox/NINAPlusPlus")]
+[assembly: AssemblyMetadata("Repository", "https://github.com/Ekrynox/CPU-GPU-Computing-for-NINA")]
 
 // The following attributes are optional for the official manifest meta data
 
 //[Optional] Your plugin homepage URL - omit if not applicaple
-[assembly: AssemblyMetadata("Homepage", "https://github.com/Ekrynox/NINAPlusPlus")]
+[assembly: AssemblyMetadata("Homepage", "https://github.com/Ekrynox/CPU-GPU-Computing-for-NINA")]
 
 //[Optional] Common tags that quickly describe your plugin
 [assembly: AssemblyMetadata("Tags", "Optimization")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/Ekrynox/NINAPlusPlus/blob/main/CHANGELOG.md")]
+[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/Ekrynox/CPU-GPU-Computing-for-NINA/blob/main/CHANGELOG.md")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "")]
