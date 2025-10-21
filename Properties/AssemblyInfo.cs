dc52ddd033bf2c15c3869628906bf0a2f0952493
Ekrynox <lucas123.a17@gmail.com>
2025-10-21 03:51:42 +0200

    Bump version to 5.2.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyVersion("0.5.2.0")]
+[assembly: AssemblyFileVersion("0.5.2.0")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("CPU\\GPU Computing for NINA")]
