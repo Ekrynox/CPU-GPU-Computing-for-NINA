ae00c2d2ac7984df65928cac7e18541c21c1227f
Ekrynox <lucas123.a17@gmail.com>
2026-06-13 12:42:50 +0200

    FastGaussianBlur: small optimization

--- CGPUNINA.Native/Image/ImageAnalysis/FastGaussianBlur.cpp
+++ CGPUNINA.Native/Image/ImageAnalysis/FastGaussianBlur.cpp
@@ -43,33 +43,87 @@ std::vector<int32_t> LucasAlias::NINA::CGPUNINA::Image::ImageAnalysis::boxesForG
 
 void LucasAlias::NINA::CGPUNINA::Image::ImageAnalysis::boxBlur_4(uint8_t* source, size_t sourceLength, uint8_t* dest, int32_t w, int32_t h, int32_t r) {
     std::memcpy(dest, source, sourceLength);
-    boxBlurH_4(dest, source, w, h, r);
-    boxBlurT_4(source, dest, w, h, r);
+
+    double iar = (double)1 / (r + r + 1);
+
+    std::vector<int32_t> indices(std::max(h, w));
+    std::iota(indices.begin(), indices.end(), 0);
+
+    std::for_each_n(std::execution::par, indices.begin(), h, [&source, &dest, w, h, r, iar](int32_t i) {
+        int32_t ti = i * w;
+        int32_t li = ti;
+        int32_t ri = ti + r;
+        uint8_t fv = dest[ti];
+        uint8_t lv = dest[ti + w - 1];
+        int32_t val = (r + 1) * fv;
+        for (int32_t j = 0; j < r; j++) val += dest[ti + j];
+        for (int32_t j = 0; j <= r; j++) {
+            val += dest[ri++] - fv;
+            source[ti++] = (uint8_t)std::floor(val * iar);
+        }
+        for (int32_t j = r + 1; j < w - r; j++) {
+            val += dest[ri++] - source[li++];
+            source[ti++] = (uint8_t)std::floor(val * iar);
+        }
+        for (int32_t j = w - r; j < w; j++) {
+            val += lv - dest[li++];
+            source[ti++] = (uint8_t)std::floor(val * iar);
+        }
+       });
+
+    std::for_each_n(std::execution::par, indices.begin(), w, [&source, &dest, w, h, r, iar](int32_t i) {
+        int32_t ti = i;
+        int32_t li = ti;
+        int32_t ri = ti + r * w;
+        uint8_t fv = source[ti];
+        uint8_t lv = source[ti + w * (h - 1)];
+        int32_t val = (r + 1) * fv;
+        for (int32_t j = 0; j < r; j++) val += source[ti + j * w];
+        for (int32_t j = 0; j <= r; j++) {
+            val += source[ri] - fv;
+            dest[ti] = (uint8_t)std::floor(val * iar);
+            ri += w;
+            ti += w;
+        }
+        for (int32_t j = r + 1; j < h - r; j++) {
+            val += source[ri] - source[li];
+            dest[ti] = (uint8_t)std::floor(val * iar);
+            li += w;
+            ri += w;
+            ti += w;
+        }
+        for (int32_t j = h - r; j < h; j++) {
+            val += lv - source[li];
+            dest[ti] = (uint8_t)std::floor(val * iar);
+            li += w;
+            ti += w;
+        }
+        });
 }
 
 void LucasAlias::NINA::CGPUNINA::Image::ImageAnalysis::boxBlurH_4(uint8_t* source, uint8_t* dest, int32_t w, int32_t h, int32_t r) {
-    auto iar = (double)1 / (r + r + 1);
+    double iar = (double)1 / (r + r + 1);
 
     std::vector<int32_t> indices(h);
     std::iota(indices.begin(), indices.end(), 0);
 
     std::for_each(std::execution::par, indices.begin(), indices.end(), [&source, &dest, w, h, r, iar](int32_t i) {
-        auto ti = i * w;
-        auto li = ti;
-        auto ri = ti + r;
-        auto fv = source[ti];
-        auto lv = source[ti + w - 1];
-        auto val = (r + 1) * fv;
-        for (auto j = 0; j < r; j++) val += source[ti + j];
-        for (auto j = 0; j <= r; j++) {
+        int32_t ti = i * w;
+        int32_t li = ti;
+        int32_t ri = ti + r;
+        uint8_t fv = source[ti];
+        uint8_t lv = source[ti + w - 1];
+        int32_t val = (r + 1) * fv;
+        for (int32_t j = 0; j < r; j++) val += source[ti + j];
+        for (int32_t j = 0; j <= r; j++) {
             val += source[ri++] - fv;
             dest[ti++] = (uint8_t)std::floor(val * iar);
         }
-        for (auto j = r + 1; j < w - r; j++) {
+        for (int32_t j = r + 1; j < w - r; j++) {
             val += source[ri++] - dest[li++];
             dest[ti++] = (uint8_t)std::floor(val * iar);
         }
-        for (auto j = w - r; j < w; j++) {
+        for (int32_t j = w - r; j < w; j++) {
             val += lv - source[li++];
             dest[ti++] = (uint8_t)std::floor(val * iar);
         }
@@ -77,33 +131,33 @@ void LucasAlias::NINA::CGPUNINA::Image::ImageAnalysis::boxBlurH_4(uint8_t* sourc
 }
 
 void LucasAlias::NINA::CGPUNINA::Image::ImageAnalysis::boxBlurT_4(uint8_t* source, uint8_t* dest, int32_t w, int32_t h, int32_t r) {
-    auto iar = (double)1 / (r + r + 1);
+    double iar = (double)1 / (r + r + 1);
 
     std::vector<int32_t> indices(w);
     std::iota(indices.begin(), indices.end(), 0);
 
     std::for_each(std::execution::par, indices.begin(), indices.end(), [&source, &dest, w, h, r, iar](int32_t i) {
-        auto ti = i;
-        auto li = ti;
-        auto ri = ti + r * w;
-        auto fv = source[ti];
-        auto lv = source[ti + w * (h - 1)];
-        auto val = (r + 1) * fv;
-        for (auto j = 0; j < r; j++) val += source[ti + j * w];
-        for (auto j = 0; j <= r; j++) {
+        int32_t ti = i;
+        int32_t li = ti;
+        int32_t ri = ti + r * w;
+        uint8_t fv = source[ti];
+        uint8_t lv = source[ti + w * (h - 1)];
+        int32_t val = (r + 1) * fv;
+        for (int32_t j = 0; j < r; j++) val += source[ti + j * w];
+        for (int32_t j = 0; j <= r; j++) {
             val += source[ri] - fv;
             dest[ti] = (uint8_t)std::floor(val * iar);
             ri += w;
             ti += w;
         }
-        for (auto j = r + 1; j < h - r; j++) {
+        for (int32_t j = r + 1; j < h - r; j++) {
             val += source[ri] - source[li];
             dest[ti] = (uint8_t)std::floor(val * iar);
             li += w;
             ri += w;
             ti += w;
         }
-        for (auto j = h - r; j < h; j++) {
+        for (int32_t j = h - r; j < h; j++) {
             val += lv - source[li];
             dest[ti] = (uint8_t)std::floor(val * iar);
             li += w;
